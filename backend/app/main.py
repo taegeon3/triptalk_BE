8559b13e39333a18a91fa58ff7770228e5586db4
taegeon3 <kimtgun7193@gmail.com>
2026-07-16 15:46:31 +0900

    됨2

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://triptalk-fe.netlify.app/",
+        "https://triptalkweb.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
