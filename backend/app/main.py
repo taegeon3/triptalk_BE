7883694a87576f26248eae61708f400ccd457696
taegeon3 <kimtgun7193@gmail.com>
2026-07-16 14:55:15 +0900

    fix: 프런트 배포 url 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://triptalk-fe.netlify.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
